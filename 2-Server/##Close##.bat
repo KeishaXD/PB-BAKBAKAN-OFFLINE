3723cba9e24448565969ebbcb0278b0ce2b07a95
KeishaXD <blackopolotong@gmail.com>
2026-04-13 11:21:58 +0700

    add Server Database

---
+++ 2-Server/##Close##.bat
@@ -0,0 +1,3 @@
+taskkill /f /im PointBlank.Auth.exe
+taskkill /f /im PointBlank.Battle.exe
+taskkill /f /im PointBlank.Game.exe
---
+++ 2-Server/##Start##.bat
@@ -0,0 +1,6 @@
+@echo off
+cd /d "%~dp0"
+
+start "" "PointBlank.Auth.exe"
+start "" "PointBlank.Battle.exe"
+start "" "PointBlank.Game.exe"
