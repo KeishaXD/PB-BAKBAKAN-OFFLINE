84871a8fa2cdb71d115add7996b30f8a925bda11
KeishaXD <blackopolotong@gmail.com>
2026-04-13 12:03:16 +0700

    add PointBlank Data Part 5

---
+++ 3-PointBlank/#STARTGAME#.bat
@@ -0,0 +1,4 @@
+@echo off
+cd /d "%~dp0"
+set idlogin=5938dc696b5a5cd3317a3f3a9dbc172d
+start "" "PointBlank.exe" /GameID:"GarenaPB" /Token:%IDLogin%
---
+++ 3-PointBlank/backup_STARTGAME.bat
@@ -0,0 +1,4 @@
+@echo off
+cd /d "%~dp0"
+set idlogin=5bca2153e5d004fdb4b3b699cdc215b4
+start "" "PointBlank.exe" /GameID:"GarenaPB" /Token:%IDLogin%
